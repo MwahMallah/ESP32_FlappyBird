45b11ec6880630667817716e4c55e43373ab0559
Maksim Dubrovin <maximus27rus@gmail.com>
2024-12-05 01:12:17 +0100

    Add more abstract classes, that incapsulate all hw logic in them. Defined game loop in update function.

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,62 @@
 #include <Arduino.h>
 #include <SPI.h>
-#include <Adafruit_GFX.h>
-#include <TFT_ILI9163C.h>
 
-// Color definitions
-#define BLACK   0x0000
-#define BLUE    0x001F
-#define RED     0xF800
-#define GREEN   0x07E0
-#define CYAN    0x07FF
-#define MAGENTA 0xF81F
-#define YELLOW  0xFFE0  
-#define WHITE   0xFFFF
+#include "Scene/Scene.hpp"
+#include "Hero/Hero.hpp"
+#include "Display/Display.hpp"
+#include "UserControl/UserControl.hpp"
 
-// Define the pin connections for ESP32
-#define __CS    5   // Chip Select pin
-#define __DC    2   // Data/Command pin
-#define __RESET 4   // Reset pin
+const int ledPin = 16;
 
-// Initialize the TFT display with the correct pins
-TFT_ILI9163C tft = TFT_ILI9163C(__CS, __DC, __RESET);
+void update(Scene& scene, Hero& hero, UserControl& control) {
+    // check for data from Bluetooth 
+    if (control.hasInput()) {
+        PlayerAction action = control.getInput();
+        
+        // handle received action
+        if (action == PlayerAction::JUMP) {
+            hero.jump();
+            Serial.println(hero.currSpeed());
+        } 
+        // else if (received == "ON") {
+        //     digitalWrite(ledPin, HIGH); // turn led on
+        // } 
+    }
 
-void setup() {
-  // Start the TFT display
-  tft.begin();
-  
-  // Optional: Set up the display (e.g., fill the screen with a color)
-  tft.fillScreen(BLACK);
-  
-  // Optional: Test the display by drawing something
-  tft.setTextColor(WHITE);
-  tft.setTextSize(1);
-  tft.setCursor(10, 10);
-  tft.print("Hello, ESP32!");
+    scene.update();
+    delay(70);
 }
 
-void loop() {
-  // Your code here (e.g., animation, graphics, etc.)
+void setup() {
+    Hero hero;
+    Scene scene(hero);
+    UserControl control;
+
+    Serial.begin(9600);
+    Serial.println("ESP32 Initialized");
+    pinMode(ledPin, OUTPUT);
+
+    // Bluetooth initialization
+    control.init();
+    Serial.println("Bluetooth Initialized");
+
+    //initialize scene with message "Connect player"
+    scene.init();
+    Serial.println("Scene Initialized");
+    scene.showMessage("Connect Player");
+
+    control.waitPlayer();
+
+    //initialize new scene without messages
+    scene.init();
+
+    //game loop
+    while (true) {
+        update(scene, hero, control);
+    }
+}
+
+void loop()
+{
+
 }
