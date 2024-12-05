6315c0a1acdf607a34c7863b57a27a841b26c3f1
Maksim Dubrovin <maximus27rus@gmail.com>
2024-12-05 01:10:11 +0100

    Added singleton display class.

---
+++ src/Display/Display.cpp
@@ -0,0 +1,4 @@
+#include "Display.hpp"
+
+// Инициализация статического указателя
+TFT_ILI9163C* Display::_instance = nullptr;
---
+++ src/Display/Display.hpp
@@ -0,0 +1,31 @@
+#ifndef __DISPLAY_HPP
+#define __DISPLAY_HPP
+
+#include <Adafruit_GFX.h>
+#include <TFT_ILI9163C.h>
+
+// pin connections for ESP32
+#define CS    5   // Chip Select pin
+#define DC    2   // Data/Command pin
+#define RESET 4   // Reset pin
+
+const int dispHeight = 128;
+const int dispWidth = 128;
+
+//Singleton class for display
+class Display {
+    static TFT_ILI9163C* _instance;
+    Display() {} // constructor is private
+
+public:
+    static TFT_ILI9163C& getInstance() {
+        if (_instance == nullptr) {
+            _instance = new TFT_ILI9163C(CS, DC, RESET); // init display
+            _instance->begin();
+        }
+        return *_instance;
+    }
+};
+
+
+#endif
